7107b0b6d12f698bd56b26f1978819b1f770b17d
Abir Khan <ak99.abirkhan@gmail.com>
2025-01-11 16:05:51 +0600

    sidebar updated

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -13,8 +13,6 @@ import { useColorMode } from "@chakra-ui/react"; // Import Chakra UI's useColorM
 const sidebarItems = [
 	{ label: "Home", to: "/", icon: <FaHome /> },
 	{ label: "Client", to: "/client", icon: <FaInfoCircle /> },
-	{ label: "Services", to: "/service-price", icon: <FaInfoCircle /> },
-
 	{
 		label: "Services",
 		to: "#",
