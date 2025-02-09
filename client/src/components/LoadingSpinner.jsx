45364dbdcb12f77369bce334e2b601814091be29
Abir Khan <ak99.abirkhan@gmail.com>
2025-02-09 22:52:39 +0600

    some fixed bugs

--- client/src/components/LoadingSpinner.jsx
+++ client/src/components/LoadingSpinner.jsx
@@ -12,11 +12,11 @@ const LoadingSpinner = () => {
 			gap={4}
 		>
 			<Spinner
-				thickness="4px"
-				speed="0.65s"
-				emptyColor="gray.200"
-				color="blue.500"
-				size="xl"
+				thickness="4px" // Border thickness
+				speed="0.65s" // Animation speed
+				emptyColor="gray.200" // Background color
+				color="blue.500" // Active color
+				size="xl" // Size (xs, sm, md, lg, xl)
 			/>
 			<Text fontSize="lg" color="gray.500">
 				Loading...
